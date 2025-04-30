d8e339de64be9c0e5f3dd5df7a2512bb3fb1d336
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-04-30 15:50:20 +0600

    middlewares added

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application, NextFunction, Request, Response } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app: Application = express();
+import status from "http-status";
+
+app.use(cors());
+app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "Green Circle Server Is Running"
+    })
+})
+
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(status.NOT_FOUND).json({
+        success: false,
+        message: "API not found",
+        error: {
+            path: req.originalUrl,
+            message: "Your requested path is not found"
+        }
+    })
+})
+
+
+
+export default app
