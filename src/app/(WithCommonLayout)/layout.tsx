73af0d3ef980b739527ba2258dfc6d22353348fa
umayermdemon <mamudmdemon@gmail.com>
2025-05-04 16:37:08 +0600

    feat: add navbar

--- src/app/(WithCommonLayout)/layout.tsx
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/shared/Navbar";
 import { ReactNode } from "react";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Navbar />
       <div className="min-h-screen">{children}</div>
     </>
   );
