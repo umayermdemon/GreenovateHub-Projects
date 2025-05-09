aea930d8b0e82d267bb1639d5df5aea4c44cce78
umayermdemon <mamudmdemon@gmail.com>
2025-05-09 20:43:13 +0600

    feat: get single & all user

---
+++ dist/app/modules/blog/blog.constant.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blogSearchableFields = exports.blogfilterableFields = void 0;
+exports.blogfilterableFields = ['title', 'description', 'status', 'searchTerm'];
+exports.blogSearchableFields = ['title'];
