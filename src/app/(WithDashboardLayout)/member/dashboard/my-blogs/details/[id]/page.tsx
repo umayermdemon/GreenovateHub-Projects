8a2c129feb5176bf99602810fa04378a47c50f08
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-05-10 11:19:36 +0600

    fix : add voting to blogDetails

--- src/app/(WithDashboardLayout)/member/dashboard/my-blogs/details/[id]/page.tsx
+++ src/app/(WithDashboardLayout)/member/dashboard/my-blogs/details/[id]/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import BlogDetailsCard from "@/components/modules/blog/BlogDetailsCard";
 import { getSingleBlog } from "@/services/blog";
 import { getSingleUSer } from "@/services/user";
+import { TAuthor, TBlog } from "@/types/blog.types";
+import { useParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
-const BlogDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
-  const { data: blog } = await getSingleBlog(id);
-  const { data: user } = await getSingleUSer(blog?.authorId);
-  return <BlogDetailsCard blog={blog} user={user} />;
+const BlogDetails = () => {
+  const { id } = useParams();
+  const [blog, setBlog] = useState<TBlog>({} as TBlog);
+  const [user, setUser] = useState<TAuthor>({} as TAuthor);
+
+  const fetchData = useCallback(async () => {
+    const res = await getSingleBlog(id);
+    if (res?.data) {
+      setBlog(res.data);
+    }
+    const user = await getSingleUSer(blog?.authorId);
+    if (user?.data) {
+      setUser(user.data)
+    }
+  }, [id, blog?.authorId])
+
+  useEffect(() => {
+
+    fetchData();
+  }, [fetchData]);
+
+  return <BlogDetailsCard blog={blog} user={user} refresh={fetchData} />;
 };
 
 export default BlogDetails;
